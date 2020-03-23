59246548d8c5c9d37e42df20f3caad01cf22b554
Joe Zhou <jzhou100@gmail.com>
2020-03-22 17:46:04 -0700

    upload the preview image into cloudinary

--- main.js
+++ main.js
@@ -3,6 +3,7 @@
 
 const encivInfo = require('./enciv-info');
 const puppeteer = require('puppeteer');
+const cloudinary = require('cloudinary').v2;
 
 global.args = {
     year: "2020",  // the year to take race data from
@@ -15,24 +16,27 @@ async function main() {
         console.info("encivInfo.init complete",Object.keys(encivInfo.viewers).length,Object.keys(encivInfo.participants).length,Object.keys(encivInfo.recorders).length);
 
         // the code to do the social media preview image should go in here 
-        // need to find which site to update or to generate new one(todo)
-        
-        //Create preview image
-        var site = 'https://undebate-stage1.herokuapp.com/schoolboard-conversation';
+        //set env variable,CC_host='https://undebate-stage1.herokuapp.com'. replace it with real host;
+        var site = process.env.CC_HOST + '/schoolboard-conversation';
+        console.log("site=" +site);
         var d = new Date();
         var image_fname = 'site_preview_'+ d.getFullYear() + '-' + d.getMonth() + '-' + d.getDate() +'.png';
         await undebate_site_preview(site, image_fname);
-
-        //Update DB (todo)
-        
+        //unload to Cloudnary
+        cloudinary.uploader.upload(image_fname, { tags: 'undebate' }, function (err, image) {
+            console.log();
+            console.log("** File Upload to Cloudinary");
+            if (err) { console.warn(err); }
+            console.log("* " + image.url);
+         });
+        //Update image.url into Iota for social media preview (under the design) 
         await encivInfo.disconnect();
     }
     catch(err){
         console.error(err);
     }
 }
-
-async function undebate_site_preview(site, image_fname) {
+ async function undebate_site_preview(site, image_fname) {
     console.log('Generate preview image for site: '+site+' image_file_name: '+image_fname);
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
